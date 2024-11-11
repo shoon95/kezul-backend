f596539a94afb744c9f569f4b074df6fa2e99dca
sanghoon <tkdgns5817@gmail.com>
2024-11-12 01:31:18 +0900

    Update: 회원 가입 엔드포인트 전체 허용

--- src/main/java/com/kezul/app/config/SecurityConfig.java
+++ src/main/java/com/kezul/app/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
                 .httpBasic((auth) -> auth.disable())
 
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/sign-up").permitAll())
+                        .requestMatchers("/auth/sign-up").permitAll())
 
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
